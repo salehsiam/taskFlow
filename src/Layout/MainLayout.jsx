79feec9f98b84fc40dbf5985f020aed5b37c945f
Abu Saleh Md Siam <abusalehmdsiam@gmail.com>
2025-02-20 23:09:22 +0600

    initial setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default MainLayout;
